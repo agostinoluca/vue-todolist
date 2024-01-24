57d71a5a6243aee4b390f1c7bc1a997bcdb16483
agostinoluca <agostino.luca89@gmail.com>
2024-01-24 18:38:48 +0100

    create array of object

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,17 @@
 <script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      toDo: [
+        { text: 'Andare dal barbiere per tagliare i capelli', done: false },
+        { text: 'Lavare il pavimento', done: false },
+        { text: 'Fare la spesa', done: false },
+        { text: 'Fare esercizi', done: true }
+      ]
+    };
+  }
+};
 
 </script>
 
