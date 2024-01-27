80f779a347e72b086b1144f771de2361e94ef5c9
agostinoluca <agostino.luca89@gmail.com>
2024-01-27 16:00:24 +0100

    add error message; some fix.

--- src/App.vue
+++ src/App.vue
@@ -6,18 +6,15 @@ export default {
     return {
       name: 'To Do List',
       newTodo: '',
+      error: '',
       toDo: [
         { text: 'Andare dal barbiere per tagliare i capelli', done: false },
-        { text: 'Lavare il pavimento', done: false },
         { text: 'Fare la spesa', done: true },
-        { text: 'Andare in palestra', done: false },
         { text: 'Lavare la macchina', done: false },
         { text: 'Inviare mail', done: false },
-        { text: 'Comprare il pane', done: false },
-        { text: 'Sbattere tappeti', done: false },
-        { text: 'Ritirare al bancomat', done: false },
         { text: 'Fare gli esercizi di Boolean', done: true }
-      ]
+      ],
+      finish: 'Non hai nulla da fare. STUDIA! 🤓'
     };
     console.log(newTodo);
   },
@@ -30,16 +27,18 @@ export default {
     removeTodo(index) {
       this.toDo.splice(index, 1);
     },
+    // aggiunge al array toDo l'input di newTodo (seguendo una condizione)
     addTodo() {
       if (this.newTodo.trim() !== '' && this.newTodo.length > 3) {
-        this.toDo.push({ text: this.newTodo, done: false})
+        this.toDo.push({ text: this.newTodo, done: false});
+        this.error = '';
       } else if (this.newTodo.length < 3) {
-        console.log('Add at least 4 characters');
+        this.error = 'Add at least 4 characters.';
       } else {
-        console.log('You entered an invalid input');
+        this.error = 'You entered an invalid input.';
       };
       this.newTodo = '';
-    }    
+    },
   }
 };
 
@@ -50,9 +49,14 @@ export default {
   <div class="card p-3 w-75 m-auto">
     <div class="d-flex justify-content-around align-items-center">
       <h1 class="text-center">{{ name }}</h1>
-      <div class="d-flex gap-1 ">
-        <input v-model="newTodo" type="text" name="newTodo" id="newTodo" placeholder="add new To Do here" @keypress.enter="addTodo">
-        <button class="p-1" @click="addTodo">Add</button>
+      <div>
+        <div class="d-flex gap-1 ">
+          <input v-model="newTodo" type="text" name="newTodo" id="newTodo" placeholder="add new To Do here" @keypress.enter="addTodo">
+          <button class="p-1" @click="addTodo">Add</button>
+        </div>
+        <div class="d-flex">
+          <span class="text-danger"> {{ error }} </span>
+        </div>
       </div>
     </div>
     <ul class="list-group fs-3" v-if="toDo.length > 0">
@@ -64,7 +68,7 @@ export default {
       </li>
     </ul>
     <div v-else>
-      <h3 class="finish fs-3 text-success">Non hai nulla da fare. STUDIA! &#128540 </h3>
+      <h3 class="finish fs-3 text-success p-5 text-center"> {{ finish }} </h3>
     </div>
   </div>
 </div>
